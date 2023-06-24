8539f43211609aea0d679aa6222707c99f805b4e
DangVanTien <tiendv29y3@gmail.com>
2023-06-25 00:39:57 +0700

    DT | fix category.

--- shop_cake/lib/src/list_food/repository/repository.dart
+++ shop_cake/lib/src/list_food/repository/repository.dart
@@ -2,6 +2,7 @@ import 'package:common/common.dart';
 
 abstract class ListFoodRepository {
   Future listFood(search);
+
   Future addFoodToOrder(foodId, quantity);
 
   Future listCategory(search);
@@ -14,32 +15,50 @@ class ListFoodRepositoryImpl implements ListFoodRepository {
 
   @override
   Future<Map<String, dynamic>> listFood(search) async {
-    final respone = await _dio.post('api/cake/getAll',queryParameters: {
-      "name":"",
-      "size": 2,
-      "page": 2,
-      "priceTo": 10,
-      "priceFrom": 100,
-      "token": ""
-    });
-    return respone.data as Map<String, dynamic>;
+    try {
+      final respone = await _dio.post(
+        '/api/cake/getAll',
+        queryParameters: {"name": "", "size": 2, "page": 2, "priceTo": 10, "priceFrom": 100, "token": ""},
+        options: Options(
+          followRedirects: false,
+          validateStatus: (status) => true,
+        ),
+      );
+      if (respone.statusCode == 200) {
+        return respone.data as Map<String, dynamic>;
+      } else {
+        throw Exception('Failed to load data!');
+      }
+    } catch (e) {
+      throw Exception('Failed to load data!');
+    }
   }
 
   @override
   Future<Map<String, dynamic>> addFoodToOrder(foodId, quantity) async {
-    final respone = await _dio
-        .post('/api/v1/carts', data: {"foodId": foodId, "quantity": quantity});
+    final respone = await _dio.post('/api/v1/carts', data: {"foodId": foodId, "quantity": quantity});
     return respone.data as Map<String, dynamic>;
   }
 
   @override
-  Future<Map<String, dynamic>> listCategory(search) async{
-    final respone = await _dio.post('api/category/getAll',queryParameters: {
-      "name":"",
-      "size": 10,
-      "page": 1,
-      "token": ""
-    });
-    return respone.data as Map<String, dynamic>;
+  Future<Map<String, dynamic>> listCategory(search) async {
+    try {
+      final respone = await _dio.post(
+        '/api/category/getAll',
+        queryParameters: {"name": "", "size": 10, "page": 1, "token": ""},
+        options: Options(
+          followRedirects: false,
+          validateStatus: (status) => true,
+        ),
+      );
+      if (respone.statusCode == 200) {
+        return respone.data as Map<String, dynamic>;
+      }
+      else {
+        throw Exception('Failed to load data!');
+      }
+    } catch (e) {
+      throw Exception('Failed to load data!');
+    }
   }
 }
